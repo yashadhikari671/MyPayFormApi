eac19a7fee019fc66ef8ac27206484b367a81c9f
Yash Adhikari <yash.adhikari@office.mypay.com.np>
2022-10-09 17:31:16 +0545

    initial commit

---
+++ MyPayFormApi/Model/Form.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPayFormApi.Model
+{
+    public class Form
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+        public string Category { get; set; }
+
+
+
+
+    }
+}
